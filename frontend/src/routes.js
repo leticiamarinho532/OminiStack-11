899c3c59ae7fdc3bd7283f417121670b235d5a48
Your Name <you@example.com>
2020-03-31 00:13:20 -0300

    Acrescentando pontuação

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Logon from './pages/Logon';
-import Register from './pages/Registry'
+import Register from './pages/Registry';
 
 export default function Routes() {
     return (
